3c3e8d0e338a6dc21479f8a86fddb16582c92a06
pedik <yapidarasinkadanil@gmail.com>
2025-02-12 23:09:54 +0700

    try

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -2,7 +2,6 @@ import ContentBox from "../ContentBox";
 import './index.scss';
 import broccoli from "../../assets/icons/broccoli.svg";
 import spicehunt from "../../assets/icons/spicehunt.svg";
-import React from "react";
 
 function MainPage() {
     return (
